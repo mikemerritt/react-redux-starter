e192eb04a242c98628d9d52df99b65261ed939ad
Mike Merritt <emmjemm@gmail.com>
2017-03-27 14:26:16 -0500

    init project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: {
+      test: /\.(js|jsx)$/,
+      include: [
+        path.resolve(__dirname, './src'),
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+        plugins: ['transform-decorators-legacy', 'transform-runtime']
+      }
+    }
+  }
+};
